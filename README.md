f15a725302c2a79af469eb3fc3183b9462234660
armandossrecife <armando.sousa@gmail.com>
2019-06-08 21:01:51 -0300

    Primeira versao da aplicacao crud-data

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# app-crud-data
-Exemplo de app-spring-boot-jpa para ser implementada em um cluster de containers docker
+# app-spring-boot-jpa
+Aplicação web CRUD usando Spring MVC com Spring Boot e Spring Data JPA.
+
+Exemplo de projeto Spring MVC usando o framework Spring Boot integrado com Spring Data JPA, BootStrap e Thymeleaf. 
+
+A aplicação foi criada usando o framework Spring MVC 4 [1] e Spring Boot 1 [2] aproveitando toda a facilidade de auto-configuração fornecida pelo Spring Boot, ou seja, não precisa fazer as configurações de aplicação. Para isso, o Spring Boot usa o conceito de convenção sobre configuração.  
+
+Na camada de visão foi usado o Thymeleaf [3] e o Bootstrap [4] em conjunto com o Jquery [5].
+
+Na camada de acesso a dados foi usado o padrão JPA [6] integrado com Hibernate. 
+
+A aplicação é estruturada no padrão Maven para aplicações web.
+
+Atualmente o projeto tem as seguintes funcionalidades:
+1. O Administrador pode gerenciar (inserir, listar, alterar e remover) uma lista de convidados. 
+2. O Administrador insere usuários da aplicação
+3. O Administrador lista usuários da aplicação 
+4. Páginas html responsivas com o uso do Bootstrap.
+
+Para rodar a aplicação:
+1. Baixe o projeto via clone da URL https://github.com/armandossrecife/app-spring-boot-jpa.git
+2. Configure o projeto como MAVEN [7].
+3. Atualize as dependências via Maven Update Project.
+4. Crie um banco mysql [8] com nome listavip.
+5. Atualize seu JDK para 1.8 
+
+Referências:
+
+[1] Spring MVC 4 - Framework Java para Aplicações Web MVC - https://docs.spring.io/spring/docs/current/spring-framework-reference/html/mvc.html
+
+[2] Spring Boot 1 - É um Framework Java (baseado na Plataforma Spring) para Aplicações web que usam inversão de contêiner de controle para a plataforma Java. https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#boot-features-security
+
+[3] Thymeleaf - É um engine de modelo Java XML / XHTML / HTML5 que pode funcionar tanto em ambientes da Web (baseados em Servlet) quanto em ambientes não-web. É mais adequado para servir XHTML / HTML5 na camada de visualização de aplicativos da web baseados em MVC, mas pode processar qualquer arquivo XML mesmo em ambientes off-line. Ele fornece integração completa do Spring Framework. https://www.thymeleaf.org
+
+[4] Bootstrap - Framework para Aplicações Web responsiva - https://v4-alpha.getbootstrap.com/getting-started/introduction
+
+[5] JQuery - Biblioteca de Funções JavaScript - https://jquery.com/
+
+[6] ORM JPA - Abstarçaõ de Acesso a Dados - https://docs.spring.io/spring-data/jpa/docs/current/reference/html/
+
+[7] Maven - Gestão de Builds e Dependências - https://maven.apache.org
+
+[8] Mysql 5 - Sistema de Gerenciamento de Banco de Dados - https://dev.mysql.com/downloads/mysql/
+
+Dúvidas, sugestões ou críticas entre em contato via e-mail armando@ufpi.edu.br
---
+++ src/main/java/com/myapp/controller/UsuarioController.java
@@ -0,0 +1,193 @@
+package com.myapp.controller;
+
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.myapp.modelo.Usuario;
+import com.myapp.services.UsuarioService;
+
+@Controller
+public class UsuarioController {
+private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	private UsuarioService usuarioService;
+		
+	/**
+	 * Contrutor
+	 */
+	public UsuarioController(){
+	}
+	
+	/**
+	 * Faz a busca de usuários
+	 * @param request HttpServletRequest do usuário 
+	 * @param model Model da aplicação
+	 * @return jsp do ModelAndView da aplicação
+	 * @throws ServletException exceção do tipo Servlet
+	 * @throws IOException exceção do tipo IOException
+	 */
+	@RequestMapping(value="/buscarUsuario", method=RequestMethod.POST)
+	protected String processarBusca(HttpServletRequest request, Model model) {
+		String tipo = request.getParameter("opcaotipo");
+		String conteudo = request.getParameter("conteudobusca");
+		List<Usuario> usuarios=null; 
+		
+		if (!conteudo.equals("")){
+			usuarios = usuarioService.buscarPorConteudo(conteudo, tipo);	
+			if (usuarios != null) {
+				Usuario usuario = usuarios.get(0); //pega apenas o primeiro
+				model.addAttribute("usuario", usuario);
+			} else {
+				model.addAttribute("mensagem", "Não retornou nenhum resultado!");
+			}
+		}else{
+			model.addAttribute("mensagem", "Conteúdo vazio...");	
+		}
+		
+		return "/resultadoBusca";
+	}
+	
+	/**
+	 * Carrega o formulário de busca de usuários
+	 * @return página TelaBuscarUsuario.jsp | Home.jsp
+	 */
+	@RequestMapping(value = "/buscarUsuario", method = RequestMethod.GET)
+	public String carregaFormularioBusca() {
+		return "/buscarUsuario";
+	}
+	
+	/**
+	 * Lista os usuários da aplicação
+	 * @param model Model da aplicação
+	 * @return página TelaListarUsuarios.jsp | Home.jsp
+	 * @throws IOException trata a exceção IOException caso aconteça
+	 */
+	@RequestMapping(value="/listarUsuarios", method=RequestMethod.GET)
+	public String processarListaUsuarios(Model model) {
+		List<Usuario> lista = usuarioService.listar();
+
+		if (lista.size() > 0 ){
+			model.addAttribute("usuarios", lista);
+		}else{
+			model.addAttribute("mensagem", "Não há usuários cadastrados no sistema");
+		}
+		return "/listarUsuarios";
+	}
+
+	/**
+	 * Faz a alteração dos dados de um usuário
+	 * @param usuario Usuario da aplicação
+	 * @param redirect RedirectAttributes
+	 * @return página TelaListarUsuarios.jsp | Home.jsp
+	 * @throws ServletException trata a exceção ServletException caso aconteça
+	 * @throws IOException trata a exceção IOException caso aconteça
+	 */
+	@RequestMapping(value="/alterarUsuario", method=RequestMethod.POST)
+	public String processarAlterarUsuario(String papel, Usuario usuario, RedirectAttributes redirectAttribute) {
+		String senhaOriginal;
+		senhaOriginal = usuario.getSenha();
+
+		usuarioService.alterar(usuario.getId(), usuario.getNome(), usuario.getLogin(), usuario.getEmail(), usuario.getSenha(), usuario.getImagemPath());
+		redirectAttribute.addFlashAttribute("mensagem", "Usuário " + usuario.getNome() + " alterado com sucesso!");
+		
+		return "redirect:/listarUsuarios";
+	}
+
+	/**
+	 * Carrega o formulário Alterar Usuário
+	 * @return página TelaAlterarUsuario.jsp | Home.jsp
+	 */
+	@RequestMapping("/alterarUsuario")
+	public ModelAndView carregaFormularioAlterar(int id) {
+		Usuario usuario = usuarioService.buscarPorId(id);
+		
+		ModelAndView mav = new ModelAndView("/alterarUsuario");
+		mav.addObject("usuario", usuario);
+		
+		return (mav);
+	}
+
+	/**
+	 * Carrega o formulário Inserir Usuário
+	 * @return página TelaInserirUsuario.jsp | Home.jsp
+	 */
+	@RequestMapping("/inserirUsuario")
+	public String carregarFormularioInserir(Model model) {
+		Usuario usuario = new Usuario();
+		
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("papel", "papel");
+		
+		return "/inserirUsuario";
+	}
+
+	/**
+	 * Insere usuário
+	 * @param usuario Dados do Usuário
+	 * @param redirect RedirectAttributes mensagem de redirect
+	 * @return página TelaPrincipal.jsp | Home.jsp
+	 * @throws ServletException 
+	 * @throws IOException
+	 */
+	@RequestMapping(value="/inserirUsuario", params={"save"})
+	public String processarInserirUsuario(final Usuario usuario, final String papel, RedirectAttributes redirectAttribute){
+		String senhaOriginal; 
+		senhaOriginal = usuario.getSenha();
+				 
+		usuarioService.inserir(usuario);
+		redirectAttribute.addFlashAttribute("mensagem", "Usuario inserido com sucesso!");
+		
+		return "redirect:/listarUsuarios";
+	}
+	
+	/**
+	 * Mostra detalhes do usuário selecionado
+	 * @param id do usuário
+	 * @return view TelaDetalhesUsuario.jsp | Home.jsp
+	 */
+	@RequestMapping("/detalharUsuario/{id}")
+	public ModelAndView processarDetalhesUsuario(@PathVariable("id") int id) {
+		ModelAndView mav = new ModelAndView("/detalharUsuario");
+		
+		Usuario usuario = usuarioService.buscarPorId(id);
+		mav.addObject("usuario", usuario);
+		
+		return mav;
+	}
+
+	/**
+	 * Remove o usuáro seleconado
+	 * @param id do usuário selecionado
+	 * @param redirectAttribute mensagem do tipo flash para evitar repetição de post
+	 * @return view listaUsuários | Home.jsp
+	 */
+	@RequestMapping("/removerUsuario/{id}")
+	public String processarRemoverUsuario(@PathVariable("id") int id, RedirectAttributes redirectAttribute) {
+		Usuario usuario = usuarioService.buscarPorId(id);
+		
+		usuarioService.remover(usuario);
+		redirectAttribute.addFlashAttribute("mensagem", "Usuario " + id + " removido com sucesso!");
+		
+		return "redirect:/listarUsuarios";
+	}
+	
+	@RequestMapping("/meuperfil/{id}")
+	public ModelAndView processarPerfilUsuario(@PathVariable("id") Integer id) {
+		ModelAndView mav = new ModelAndView("/perfilUsuario");
+		//TODO conclui a funcionalidade de exibição do perfil do usuário
+		return mav;
+	}
+
+}

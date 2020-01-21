dc2ff2cee8eb4a85b0b0cdad906a01627468f157
armandossrecife <armando.sousa@gmail.com>
2020-01-21 10:23:17 -0300

    Foi criada a estrutura necessária para carregar mensagens do resource.

--- src/main/java/com/myapp/SpringBootWebApplication.java
+++ src/main/java/com/myapp/SpringBootWebApplication.java
@@ -2,10 +2,38 @@ package com.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 //http://www.thymeleaf.org/doc/articles/layouts.html
 @SpringBootApplication
 public class SpringBootWebApplication {
+	
+	/**
+	 * Bean responsável por tratar as mensagens da aplicacao
+	 * @return
+	 */
+	@Bean
+	public MessageSource messageSource() {
+	    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+	    messageSource.setBasename("classpath:messages");
+	    messageSource.setCacheSeconds(10); //reload messages every 10 seconds
+	    return messageSource;
+	}
+	
+	/**
+	 * Bean que registra o local onde as mensagens da aplicacao ficam
+	 * @param messageSource
+	 * @return
+	 */
+	@Bean
+	 public LocalValidatorFactoryBean validator(MessageSource messageSource) {
+	    LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+	    bean.setValidationMessageSource(messageSource);
+	    return bean;
+	 }
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringBootWebApplication.class, args);
